b3bec29afce53ac8dc26c5d2c93bfacb2e8dc27d
pr0m1th3as <abertsatos@biol.uoa.gr>
2025-06-13 14:07:44 +0300

    fix calendarDuration and duration arrays to handle 'end' keyword in subsref and subsasgn methods

--- inst/calendarDuration.m
+++ inst/calendarDuration.m
@@ -1129,11 +1129,23 @@ classdef calendarDuration
 ################################################################################
 ##                             Available Methods                              ##
 ##                                                                            ##
-## 'subsref'          'subsasgn'                                              ##
+## 'end'              'subsref'          'subsasgn'                           ##
 ##                                                                            ##
 ################################################################################
 
   methods (Hidden)
+
+    ## Overloaded end keyword
+    function last_index = end (this, end_dim, ndim_obj)
+      lastdim = ndims (this);
+      if (end_dim == ndim_obj && ndim_obj == 1)
+        last_index = prod (size (this));
+      elseif (end_dim == ndim_obj && end_dim < lastdim)
+        last_index = prod (size (this)(end_dim:lastdim));
+      else
+        last_index = size (this, end_dim);
+      endif
+    endfunction
 
     ## Class specific subscripted reference
     function varargout = subsref (this, s)
