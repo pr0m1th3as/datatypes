075f58acdd9b9929690abe8e0fcc567f2993a692
pr0m1th3as <abertsatos@biol.uoa.gr>
2025-09-28 11:57:08 +0300

    categorical: updaet documentation

--- inst/categorical.m
+++ inst/categorical.m
@@ -138,7 +138,7 @@ classdef categorical
     ## appearance in input array @var{A}, when @var{valueset} is not specified,
     ## in which case the unique values in @{A} are not sorted in order to set
     ## the categories.  Ordinal categorical arrays allow for relational
-    ## operators such as @qcode{>=, >, <=, <), as well as statistical operations
+    ## operators such as @qcode{>=, >, <=, <}, as well as statistical operations
     ## such as @code{min}, @code{max}, and @code{median}.
     ##
     ## @item @qcode{"Protected"} must be a logical scalar specifying that the
