b9a222609c4d12eb675217de86c2e8d0cf8caf62
pr0m1th3as <abertsatos@biol.uoa.gr>
2025-10-03 19:37:03 +0300

    remove duplicate methods

--- inst/string.m
+++ inst/string.m
@@ -1326,67 +1326,6 @@ classdef string
   endmethods
 
 ################################################################################
-##                           ** Hash Operations **                            ##
-################################################################################
-##                             Available Methods                              ##
-##                                                                            ##
-## 'keyHash'          'keyMatch'                                              ##
-##                                                                            ##
-################################################################################
-
-  methods (Access = public)
-
-    ## -*- texinfo -*-
-    ## @deftypefn {string} {@var{hey} =} keyHash (@var{C})
-    ##
-    ## Generate a hash code for string array.
-    ##
-    ## @code{@var{h} = keyHash (@var{C})} generates a @qcode{uint64} scalar that
-    ## represents the input array @var{C}.  @code{keyHash} utilizes the 64-bit
-    ## variant of the Fowler-Noll-Vo non-cryptographic hash function.
-    ##
-    ## Note that unlike MATLAB, this implementation does no use any random seed.
-    ## As a result, @code{keyHash} will always generate the exact same hash key
-    ## for any particular input across different workers and Octave sessions.
-    ##
-    ## @end deftypefn
-    function key = keyHash (this, base = [])
-      ## Initialize string with size and class name
-      size_str = sprintf ('%dx', size (this.strs))(1:end-1);
-      init_str = [size_str 'string'];
-      if (base)
-        key = __ckeyHash__(init_str, base);
-      else
-        key = __ckeyHash__(init_str);
-      endif
-      strs = [this.strs{:}];
-      key = __ckeyHash__(strs, key);
-      key = __nkeyHash__(this.isMissing(:), key);
-    endfunction
-
-    ## -*- texinfo -*-
-    ## @deftypefn {string} {@var{TF} =} keyMatch (@var{C1}, @var{C2)
-    ##
-    ## Return true if both inputs have the same hash key.
-    ##
-    ## @code{@var{TF} = keyMatch (@var{C1}, @var{C2})} returns a logical scalar,
-    ## which is @qcode{true}, if both categorical arrays @var{C1} and @var{C2}
-    ## have the same hash key, and @qcode{false} otherwise.
-    ##
-    ## @end deftypefn
-    function TF = keyMatch (A, B)
-      if (any (class (A) != class (B)))
-        TF = false;
-      else
-        A_key = keyHash (A);
-        B_key = keyHash (B);
-        TF = A_key == B_key;
-      endif
-    endfunction
-
-  endmethods
-
-################################################################################
 ##                  ** Reference and Assignment Operations **                 ##
 ################################################################################
 ##                             Available Methods                              ##
